539f0be9fb93f5d26baaf32622321f2e5af31155
DushkinaEL <dushkina.katya24@mail.ru>
2023-09-28 15:50:25 +0300

    Added image in top menu.Added textView with autolink. Added button, (1 var)fun initBtn + onClick

--- app/src/main/java/ru/dushkina/application/MainActivity.kt
+++ app/src/main/java/ru/dushkina/application/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.dushkina.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            setContentView(R.layout.activity_main)
+        }
+
+
+     fun initButtonsMenu(view: View) {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+    fun initBtnFavorite(view: View){
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+    fun initBtnWatchLater(view: View){
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+    fun initBtnSelection(view: View) {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+    fun initBtnSettings(view: View){
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
-}
+
