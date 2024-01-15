2d99164302e899af97ed57b9fa067f7b168b7561
DushkinaEL <dushkina.katya24@mail.ru>
2024-01-15 21:00:49 +0300

    module_24 commit

--- app/src/main/java/ru/dushkina/application/FilmListRecyclerAdapter.kt
+++ app/src/main/java/ru/dushkina/application/FilmListRecyclerAdapter.kt
@@ -1,14 +1,26 @@
 package ru.dushkina.application
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView.OnItemClickListener
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBindings
+import ru.dushkina.application.databinding.ActivityDetailsBinding
+import ru.dushkina.application.databinding.ActivityMainBinding
 import ru.dushkina.application.databinding.FilmItemBinding
 
 class FilmListRecyclerAdapter(private val clickListener:OnItemClickListener): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val items = mutableListOf<Film>()
+
+    private var _binding: FilmItemBinding? = null
+    private val binding get() = _binding!!
+    fun onCreate(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FilmItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return FilmViewHolder(FilmItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
 
@@ -23,6 +35,9 @@ class FilmListRecyclerAdapter(private val clickListener:OnItemClickListener): Re
             is FilmViewHolder -> {
 
                 holder.bind(items[position])
+                binding.itemContainer.setOnClickListener {
+                    clickListener.click(items[position])
+                }
             }
         }
     }
