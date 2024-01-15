784bd111fef1fb80b21a34c60f1e954743e5d341
DushkinaEL <dushkina.katya24@mail.ru>
2024-01-15 21:00:49 +0300

    fixed the errors, now the application does not crash

--- app/src/main/java/ru/dushkina/application/Film.kt
+++ app/src/main/java/ru/dushkina/application/Film.kt
@@ -1,8 +1,11 @@
 package ru.dushkina.application
 
-data class Film (
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Film(
     val title: String,
     val poster: Int,
     val description: String
-
-)
+) : Parcelable
