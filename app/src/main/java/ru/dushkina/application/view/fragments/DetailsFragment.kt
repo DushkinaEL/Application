a50460ec335a8f6b49d05ecdc27e29516669261a
DushkinaEL <dushkina.katya24@mail.ru>
2024-06-21 23:17:10 +0300

    43 модуль не показывает постер и не загружает его в галерею

--- app/src/main/java/ru/dushkina/application/view/fragments/DetailsFragment.kt
+++ app/src/main/java/ru/dushkina/application/view/fragments/DetailsFragment.kt
@@ -129,7 +129,7 @@ class DetailsFragment : Fragment() {
             //Выводим снекбар с кнопкой перейти в галерею
             Snackbar.make(
                 binding.root,
-                R.string.downloaded_to_gallery
+                R.string.downloaded_to_gallery,
                 Snackbar.LENGTH_LONG
             )
                 .setAction(R.string.open) {
@@ -191,7 +191,7 @@ class DetailsFragment : Fragment() {
             //Открываем канал для записи на диск
             val outputStream = contentResolver.openOutputStream(uri!!)
             //Передаем нашу картинку, может сделать компрессию
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream?.let { bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it) }
             //Закрываем поток
             outputStream?.close()
         } else {
